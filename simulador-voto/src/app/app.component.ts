89a8f0bb097550e5d3aef160dfdb9141c87c124e
HolySpirit777 <43834329+HolySpirit777@users.noreply.github.com>
2019-09-08 22:46:07 -0400

    Base 3.2

--- simulador-voto/src/app/app.component.ts
+++ simulador-voto/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simulador-voto';
+
+  mostrar = true;
+  etiqueta = 'Elije el partido';
+
+  clickMostrar() {
+    this.mostrar = !this.mostrar;
+    if (this.mostrar === false) {
+      this.etiqueta = 'Elige al candidato de su preferencia';
+    } else {
+      this.etiqueta = 'Elije el partido';
+    }
+  }
 }
